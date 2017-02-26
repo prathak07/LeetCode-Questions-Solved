df276a61e9ab5967414b9f02cf7c083a348bee09
Prathak Rastogi <prathak07@gmail.com>
2017-02-26 02:50:17 -0500

    Update Remove Invalid Parentheses

--- Solutions/Remove Invalid Parentheses.java
+++ Solutions/Remove Invalid Parentheses.java
@@ -15,24 +15,33 @@ public class Solution {
         List<String> res = new ArrayList<>(); // result
         
         Queue<String> q = new LinkedList<>(); // for BFS
+        Queue<Integer> levelQ = new LinkedList<>(); // for level of tree
         Set<String> visited = new HashSet<>(); // to mark visited Strings
         
         q.offer(s);
+        levelQ.offer(0);
         visited.add(s);
+        
+        int max = 0;
         
         boolean found = false; // flag to keep track we have found valid parentheses at some level so no need to add more strings
         
         while(!q.isEmpty()) {
             String str = q.poll();
-            
+            int level = levelQ.poll();
             if(validParentheses(str)) {
                 res.add(str);
                 found = true;
+                max = level;
             }
-            
+        
             // if the parentheses is already found then continue to remaining entries left in queue and don't create another level of entries for the queue
             if(found) { 
                 continue;
+            }
+            
+            if(found && max<level) {
+                break;
             }
             
             // this loop creates successive strings with less parentheses thus creating levels for BFS
@@ -45,6 +54,7 @@ public class Solution {
                 
                 if(!visited.contains(t)) {
                     q.offer(t);
+                    levelQ.offer(level+1);
                     visited.add(t);
                 }
             }
